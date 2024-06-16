7a45ae29ec9a1b2f33cfa838bb6e9f9132b985d4
Kenny Marshall <kenny.mrshll@gmail.com>
2024-06-16 19:26:43 +0200

    boost command line args

---
+++ README.md
@@ -0,0 +1,3 @@
+An incomplete rewriting of my xas_normalisation repository to C++ as an exercise for myself. Currently does file splitting/ column selection and regridding, and does run a bit faster, but does not do the normalisation. 
+
+Uses Boost library.
---
+++ src/columnExtraction_thetaCorrection.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <tuple>
+#include "columnExtractionFunctions.cpp"
+#include <map>
+#include <boost/program_options.hpp>
+#include <filesystem>
+using namespace std;
+namespace po = boost::program_options;
+
+
+
+int main(int argc, char *argv[]){
+po::options_description desc("Allowed options");
+
+desc.add_options()
+    ("help", "produce help message")
+    ("thetaOffset", po::value<float>(), "set theta offset on monochromator")
+    ;
+
+po::variables_map vm;
+float thetaOffset;
+
+po::store(po::parse_command_line(argc,argv,desc),vm);
+po::notify(vm);
+if (vm.count("help")){
+    cout << desc << endl;
+    return 0;
+}
+
+if (vm.count("thetaOffset")){
+    thetaOffset = vm["thetaOffset"].as<float>();
+    
+}
+else {
+    thetaOffset = 0;
+}
+
+cout << "thetaOffset=" << thetaOffset;
+
+
+
+string directory; 
+if (argc == 2){
+ directory = argv[1];
+}
+else {
+directory = ".";
+}
+cout << directory << endl;
+
+
+//tuple<vector<vector<float>> , vector<string>> arrayandHeaders = datToVector(file, counterNames);
+    map<string,filesystem::file_time_type> fileTimeMap;
+    map<string, int> fileScanMap;
+    for (auto entry : filesystem::directory_iterator(directory)){
+
+        string file = entry.path().string();
+        string dirname = entry.path().parent_path().string();
+        
+        
+        if (isIn(file,".dat")){
+            cout << file << endl;
+            int noscans = getLastScan(file);
+            fileScanMap[file] = noscans;
+            cout << "total scans " << noscans <<"\n";
+            processFile(file,thetaOffset);
+        }
+        filesystem::file_time_type mtime = filesystem::last_write_time(entry);
+        fileTimeMap[file] = mtime;
+        
+    }
+    //return fileScanMap;
+
+for (auto entry : filesystem::directory_iterator(directory+"/columns/")){
+    if (filesystem::is_directory(entry)){
+    string rgdir = entry.path().string();
+
+
+    regridFiles(rgdir+"/");
+    }
+    }
+}
